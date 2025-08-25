26603f78819992a1db651f6cdf6ef35e8ace55ae
Elias729 <eliashossainrafe72@gmail.com>
2025-08-26 01:47:33 +0600

    some update and js code done for login form

--- js/payoo.js
+++ js/payoo.js
@@ -1,1 +1,18 @@
-console.log("PAYOO Mobile Banking");
+// * Login page functionality
+document.getElementById("login-button").addEventListener("click", (e) => {
+    e.preventDefault();
+    const mobileNumber = "01820627082";
+    const pin = "2004";
+
+    const mobileNumberInput = document.getElementById("mobile-number");
+    const mobileNumberValue = mobileNumberInput.value;
+    mobileNumberInput.value = "";
+    const pinInput = document.getElementById("pin");
+    const pinValue = pinInput.value;
+    pinInput.value = "";
+    if (mobileNumberValue === mobileNumber && pinValue === pin) {
+        window.location.href = "../home.html";
+    } else {
+        alert("Invalid mobile number or PIN");
+    }
+})
